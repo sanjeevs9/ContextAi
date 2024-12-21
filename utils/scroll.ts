3ac0dcd883a3bb32673ab63297c0b5f9a70a6389
Sanjeev <sanjeev.19kr@gmail.com>
2024-12-21 15:20:14 +0530

    remove everything

--- utils/scroll.ts
+++
@@ -1,6 +0,0 @@
-export const scrollToSection = (sectionId: string) => {
-  const element = document.getElementById(sectionId);
-  if (element) {
-    element.scrollIntoView({ behavior: 'smooth' });
-  }
-};
